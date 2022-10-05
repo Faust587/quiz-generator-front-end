9264eb874ae5818a1b046c180c0a3d45b3d59d57
Yura Synkulych <kri3g0vec@gmail.com>
2022-10-05 15:31:23 +0300

    Created main page

--- src/components/forms/SearchForm/SearchForm.tsx
+++ src/components/forms/SearchForm/SearchForm.tsx
@@ -1,8 +1,28 @@
+import "./SearchFormStyles.scss";
+
 export const SearchForm = () => {
 
   return (
-    <>
-
-    </>
+    <div className="search-form-container">
+      <h1 className="search-form-title">
+        Enter your quiz code
+      </h1>
+      <h2 className="search-form-subtitle">
+        To start a new quiz
+      </h2>
+      <form className="search-form">
+        <input
+          type="text"
+          className="search-form-input"
+          placeholder="Ex: cS09S"
+        />
+        <button
+          type="submit"
+          className="search-form-submit-button"
+        >
+          Enter
+        </button>
+      </form>
+    </div>
   );
 };
--- src/components/headers/MainHeader/MainHeader.tsx
+++ src/components/headers/MainHeader/MainHeader.tsx
@@ -1,10 +1,29 @@
+import "./MainHeaderStyles.scss";
+import userIcon from "../../../assets/icons/user-icon.svg";
 import { SearchForm } from "../../forms/SearchForm/SearchForm";
 
 export const MainHeader = () => {
 
   return (
-    <>
-      <SearchForm />
-    </>
+    <div className="main-header-container">
+      <header className="main-header">
+        <div className="main-header-top-section">
+          <div className="main-header-titles-container">
+            <h1 className="main-header-title">
+              Let's play
+            </h1>
+            <h2 className="main-header-subtitle">
+              And be the first!
+            </h2>
+          </div>
+          <img
+            alt="user-profile"
+            src={ userIcon }
+            className="main-header-profile-icon"
+          />
+        </div>
+        <SearchForm />
+      </header>
+    </div>
   );
 };
