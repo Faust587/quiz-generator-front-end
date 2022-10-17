c3d5388deb53f07c93fa3778a096f054faa12205
Yura Synkulych <kri3g0vec@gmail.com>
2022-10-17 10:18:15 +0300

    Deleted unused code

--- src/components/headers/GeneratorHeader/GeneratorHeader.tsx
+++ src/components/headers/GeneratorHeader/GeneratorHeader.tsx
@@ -17,37 +17,7 @@ export const GeneratorHeader: FC<propTypes> = (
 
   return (
     <div className="generator-header-container">
-      <section className="generator-header-top-section">
-        <input
-          className="generator-header-top-section__quiz-name"
-          value={ quizName }
-          size={ quizName.length - 3 } // perfect for input size
-          maxLength={ 20 }
-          minLength={ 1 }
-          onChange={ e => setQuizName(e.target.value) }
-          type="text"
-        />
-        <button className="generator-header-top-section__home-button">
-          <img
-            src={ userLogo }
-            alt="Home page"
-          />
-        </button>
-      </section>
-      <nav className="generator-header-navbar">
-        <div
-          className={ `generator-header-navbar__question ${ page ? "active" : "inactive" }` }
-          onClick={ () => setPage(!page) }
-        >
-          Questions
-        </div>
-        <div
-          className={ `generator-header-navbar__settings ${ page ? "inactive" : "active" }` }
-          onClick={ () => setPage(!page) }
-        >
-          Settings
-        </div>
-      </nav>
+
     </div>
   );
 };
