9bb17c1a3a62603dfe3495410916f41623dfadd5
Yura Synkulych <kri3g0vec@gmail.com>
2022-12-27 10:36:18 +0200

    deleted deprecated generator page

--- src/pages/GeneratorPage/GeneratorPage.tsx
+++ src/pages/GeneratorPage/GeneratorPage.tsx
@@ -1,26 +1,9 @@
-import "./GeneratorPageStyles.scss";
-import { GeneratorHeader } from "../../components/headers/GeneratorHeader/GeneratorHeader";
-import { useState } from "react";
-import { GeneratorNavBar } from "../../components/navBars/GeneratorNavBar/GeneratorNavBar";
-import { QuestionGenerator } from "../../components/questionGenerator/QuestionGenerator";
+import styles from "./GeneratorPage.module.scss";
 
 export const GeneratorPage = () => {
-  const [ quizName, setQuizName ] = useState("Quiz name");
-  const [ page, setPage ] = useState(true);
-
   return (
-    <div className="generator-page-container">
-      <GeneratorHeader
-        quizName={ quizName }
-        setQuizName={ setQuizName }
-      />
-      <GeneratorNavBar
-        page={ page }
-        setPage={ setPage }
-      />
-      {
-        page ? <QuestionGenerator /> : "SHIT"
-      }
+    <div>
+
     </div>
   );
 };
